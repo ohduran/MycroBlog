0dba6f73bfc89c2cf2f7a8ac58b7e21043b6a081
ohduran <alvaro.duranb@gmail.com>
2017-07-07 13:51:31 +0100

    Removed deprecated "Form" and replaced with "FlaskForm".

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,19 @@
-"""Forms structure for Login, Registering and Editing."""
+"""Forms structure for Login and Editing user."""
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
+    """Login Form."""
+
     username = StringField("Name", validators=[DataRequired()])
     email = StringField("Email")
     password = StringField("Password", validators=[DataRequired()])
 
 
 class EditForm(FlaskForm):
+    """Edit user's profile Form."""
+
     username = StringField('username', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=200)])
