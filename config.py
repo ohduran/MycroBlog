9a088275c7a20a1dccd921582c0e29a422a9d40b
ohduran <alvaro.duranb@gmail.com>
2017-07-21 00:42:50 +0100

    Security ammendments.

--- config.py
+++ config.py
@@ -11,15 +11,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Mail server settings
-
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_TSL = False
-MAIL_USE_SSL = True
-MAIL_DEFAULT_SENDER = 'mycroblogduran@gmail.com'
-MAIL_USERNAME = os.environ.get('mycroblogduran@gmail.com')
-MAIL_PASSWORD = os.environ.get('09111991')
 
 # administrator list
 
